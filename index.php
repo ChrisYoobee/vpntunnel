ca3fac637e427ec098588cd4f5b916ca989860bd
Chris <example@example.com>
2017-12-07 11:48:50 +1300

    Services section working and nearlly completed

--- index.php
+++ index.php
@@ -40,8 +40,8 @@
     			        <div class="row">
                             <?php
                                     $parms2 = array(
-                                    'post_type'=>'vpn',
-                                );
+                                        'post_type'=>'vpn',
+                                    );
                                 $VPN = new WP_Query($parms2);
                              ?>
                             <?php if($VPN->have_posts()): ?>
@@ -59,7 +59,7 @@
 		       </div>
 	      </div>
 
-          </div>
+    </div>
 
 </div> <!-- First Row -->
 
@@ -127,8 +127,24 @@
 </div>
 
 <div class="vpn-services">
+
     <div class="row v-align">
+        <?php
+                $parms = array(
+                'type'=>'post',
+                'category_name'=>'services-title'
+            );
+            $vpn = new WP_Query($parms);
+         ?>
+        <?php if($vpn->have_posts()): ?>
+            <?php while($vpn->have_posts()): $vpn->the_post();?>
+                <h3 class="services-title"><?php the_title(); ?></h3>
+            <?php endwhile; ?>
+        <?php endif; ?>
         <div class="col-md-10 no-float">
+
+
+
 
             <?php
                     $parms2 = array(
@@ -141,11 +157,22 @@
                 <div class="col-md-4 col-xs-12">
                     <div class="basic-service">
                         <div class="service-top-section">
-                            <h2 class="v-align">Basic</h2>
+                            <h2 class="v-align"><?php the_title(); ?></h2>
                         </div>
-                        <h3><?php the_title(); ?></h3>
-                        <div><?php $meta_value = get_post_meta($post->ID, 'Price'); ?></div>
-                        <div><?php the_content(); ?></div>
+                        <div class="services-content">
+                            <div class="service-time"><h4><?php echo get_post_meta( $post->ID, 'subscriptionInfo', true ); echo $my_meta["subscriptionInfo"]; ?></h4></div>
+                            <div><h2><?php echo get_post_meta( $post->ID, 'price', true ); echo $my_meta["price"]; ?><h2></div>
+                            <p>Per Month</p>
+                            <div class="conditions">
+                            <?php echo get_post_meta( $post->ID, 'billingConditions', true ); echo $my_meta["billingConditions"]; ?>
+                            <h4>30 Day Money Back Guarentee</h4>
+                            </div>
+                        </div>
+                        <div class="btn-primary header-button service-btn">
+                            <h3>Get <?php echo get_post_meta( $post->ID, 'subscriptionInfo', true ); echo $my_meta["subscriptionInfo"]; ?></h3>
+                        </div>
+
+
                     </div>
                 </div>
             <?php endwhile; ?>
